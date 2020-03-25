474854a1f351cdf59b3f1387164ad0c55b74c091
andrei1299 <47647062+andrei1299@users.noreply.github.com>
2020-03-25 17:42:12 +0200

    Add files via upload

---
+++ ClassLibrary/ClassLibrary/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test Model Designer First");
+            TestPerson();
+            Console.ReadKey();
+        }
+        static void TestPerson()
+        {
+            using (ModelClassLibraryContainer context = new ModelClassLibraryContainer())
+            {
+                Comment p = new Comment()
+                {
+                    Text = "Experimental Text",
+                    PostPostId = "1322"
+                };
+                context.Comments.Add(p);
+                context.SaveChanges();
+                var items = context.Comments;
+                foreach (var x in items)
+                    Console.WriteLine("{0} {1}", x.Text, x.PostPostId);
+            }
+        }
+    }
+
+}
